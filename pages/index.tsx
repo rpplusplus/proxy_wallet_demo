a3a8dd7628f17bbe9e388b66a78c180a7e0ea2c8
txx <tangxiaoxuan2005@gmail.com>
2023-09-01 02:39:17 +0800

    style(pages/index.tsx): remove unused import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,9 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { LocalWallet } from "@thirdweb-dev/wallets";
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-import { Mumbai } from "@thirdweb-dev/chains"
-import { generateMnemonic, mnemonicToEntropy, entropyToMnemonic } from "bip39";
-import { useSession, signIn } from 'next-auth/react';
-import NotLogin from './components/not_login';
-import NoWallet from './components/no_wallet';
-import { User } from '@prisma/client';
-import { Main } from 'next/document';
 import MainBody from './components/main';
 
 const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
 
-  const { data: session } = useSession()
   return (<>
     <header className="navbar bg-base-100">
       <a className="btn btn-ghost normal-case text-xl">Proxy Wallet Demo</a>
